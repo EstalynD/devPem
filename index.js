aa45c1d2827348beb0420e25fa5408c17c0e7978
1428960@senati.pe <camtherfullacc@gmail.com>
2025-04-25 17:46:41 -0500

    create endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const fs = require('fs')
 const path = require('path')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
 const certPath = path.join(__dirname, './10472888281_Mnlmmn08.p12')
 const dev_signature = fs.readFileSync(certPath)
 
@@ -12,8 +16,6 @@ const signValidation = require('./signValidation')
 
 const pem = require('pem')
 if (process.env.NODE_ENV !== 'production') pem.config({ pathOpenSSL: './bin/openssl' }) // en windows fallará
-
-
 
 const UBL_EXTENSION_NAMESPACE = (
     'urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2'
@@ -27,9 +29,6 @@ const XPATH_TO_SIGNATURE_CONTAINER = (
     '/*[local-name(.)="ExtensionContent" and ' +
     'namespace-uri(.)="' + UBL_EXTENSION_NAMESPACE + '"][last()]'
 )
-
-
-
 
 const xml = `
 <?xml version="1.0" encoding="utf-8"?>
@@ -148,47 +147,50 @@ const xml = `
 `
 
 const xmlFileString = xml
-// console.log('xmlFileString', xmlFileString)
 
-const signature = dev_signature
-const pass = 'Mnlmmn08'
+app.get('/PemApisunat', (req, res) => {
+    const signature = dev_signature
+    const pass = 'Mnlmmn08'
 
-pem.readPkcs12(signature, { p12Password: pass }, (error, pem) => {
-    if (error) {
-        console.log('signature', signature)
-        console.log('pass', pass)
-        console.log('error', error)
-        return console.log('reject', { ...error, message: 'error del certificado o la contraseña de este' })
-    }
+    pem.readPkcs12(signature, { p12Password: pass }, (error, pem) => {
+        if (error) {
+            console.log('signature', signature)
+            console.log('pass', pass)
+            console.log('error', error)
+            return res.status(400).json({ ...error, message: 'error del certificado o la contraseña de este' })
+        }
 
-    let sig = new SignedXml()
+        let sig = new SignedXml()
 
-    sig.addReference(
-        '/*', // referencia al nodo a firmar (en este caso el nodo principal, se firma todo el documento)
-        [
-            'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
-            'http://www.w3.org/2001/10/xml-exc-c14n#' // este transform hace <NodoVacio></NodoVacio>
-        ],
-        'http://www.w3.org/2000/09/xmldsig#sha1', // digestAlgorithm
-        '',
-        '',
-        '',
-        true // Deja vacio el atributo URI del nodo firmado
-    )
-    sig.signingKey = pem.key
-    sig.keyInfoProvider = new KeyInfoProvider(pem.cert)
-    sig.computeSignature(xmlFileString.replace(/\r/g, ''), {
-        prefix: 'ds',
-        attrs: { Id: 'APISUNAT' },
-        location: { reference: XPATH_TO_SIGNATURE_CONTAINER, action: "append" }
+        sig.addReference(
+            '/*',
+            [
+                'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
+                'http://www.w3.org/2001/10/xml-exc-c14n#'
+            ],
+            'http://www.w3.org/2000/09/xmldsig#sha1',
+            '',
+            '',
+            '',
+            true
+        )
+        sig.signingKey = pem.key
+        sig.keyInfoProvider = new KeyInfoProvider(pem.cert)
+        sig.computeSignature(xmlFileString.replace(/\r/g, ''), {
+            prefix: 'ds',
+            attrs: { Id: 'APISUNAT' },
+            location: { reference: XPATH_TO_SIGNATURE_CONTAINER, action: "append" }
+        })
+
+        let signedXmlString = sig.getSignedXml()
+
+        let isValid = signValidation(pem, signedXmlString, XPATH_TO_SIGNATURE_CONTAINER)
+        if (isValid !== true) return res.status(400).json(isValid)
+
+        res.json({ signedXml: signedXmlString })
     })
+})
 
-    let signedXmlString = sig.getSignedXml()
-
-    // validar firma / buscar TRUE
-    let isValid = signValidation(pem, signedXmlString, XPATH_TO_SIGNATURE_CONTAINER)
-    if (isValid !== true) return console.log('reject', isValid)
-
-    // devolver xml firmado
-    console.log('resolve', signedXmlString)
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`)
 })
